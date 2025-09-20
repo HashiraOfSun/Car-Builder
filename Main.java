e5e06644c890ccc05f854bf6b17ea9343b6b9fd5
Nursultan Makhmutov <241564@astanait.ed.kz>
2025-09-20 13:16:45 +0500

    Added Director class to handle Car building, updated Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car.CarBuilder()
-                .setBrand("Toyota")
-                .setModel("Camry")
-                .setYear(2022)
-                .setColor("Black")
-                .setAutomatic(true)
-                .build();
+        Director director = new Director();
 
+        Car car = director.buildToyotaCamry();
         System.out.println(car);
+
+        Car car2 = director.buildBMWX5();
+        System.out.println(car2);
     }
 }
